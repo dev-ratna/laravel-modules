409f15140f9671ed01be4d5529d5c0bb16edddea
Hunter Skrasek <hunter@perk.com>
2014-12-17 10:50:09 -0600

    Initial config file and ServiceProvider cleanup
    Signed-off-by: Hunter Skrasek <hunter@perk.com>

--- src/Perk/LaravelModules/LaravelModulesServiceProvider.php
+++ src/Perk/LaravelModules/LaravelModulesServiceProvider.php
@@ -2,43 +2,43 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class LaravelModulesServiceProvider extends ServiceProvider {
+class LaravelModulesServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('perk/laravel-modules');
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('perk/laravel-modules');
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
-
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 }
---
+++ src/config/config.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+    /**
+     * The installation path for newly generated modules.
+     */
+    'path'     => 'app/modules',
+    /**
+     * List of files to be included by each module.
+     */
+    'includes' => array(
+        'helpers.php',
+        'routes.php',
+        'filters.php'
+    )
+);
