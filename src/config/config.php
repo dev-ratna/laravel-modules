6490f1b63871f820445e40eb63532380cee58483
Hunter Skrasek <hunter@perk.com>
2014-12-17 13:48:47 -0600

    Update config file to use app_path and add illuminate/filesystem and mustache to dependencies.
    Signed-off-by: Hunter Skrasek <hunter@perk.com>

--- src/config/config.php
+++ src/config/config.php
@@ -3,7 +3,7 @@ return array(
     /**
      * The installation path for newly generated modules.
      */
-    'path'     => 'app/modules',
+    'path'     => app_path('modules'),
     /**
      * List of files to be included by each module.
      */
